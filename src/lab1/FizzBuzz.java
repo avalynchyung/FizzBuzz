826e02491c2842dfe0f0aa9625c8145eb9ea6ebb
ascodes17 <ava.chyung@mail.utoronto.ca>
2026-09-15 14:30:36 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -11,30 +11,35 @@ class FizzBuzz {
 //        for (int i = 1; i <= 100; i++) {
 
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-            Result result = new Result(divisibleBy3, divisibleBy5);
-
-            // Print our appropriate result.
-            if (result.divisibleBy3() && result.divisibleBy5()) {
-
-                System.out.println("Fizz Buzz");
-
-            } else if (result.divisibleBy3()) {
-
-                System.out.println("Fizz");
-
-            } else if (result.divisibleBy5()) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
-            i++;
+            i = doFizzBuzz(i);
         }
+    }
+
+    private static int doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+        Result result = new Result(divisibleBy3, divisibleBy5);
+
+        // Print our appropriate result.
+        if (result.divisibleBy3() && result.divisibleBy5()) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (result.divisibleBy3()) {
+
+            System.out.println("Fizz");
+
+        } else if (result.divisibleBy5()) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
+        i++;
+        return i;
     }
 
     private record Result(boolean divisibleBy3, boolean divisibleBy5) {
